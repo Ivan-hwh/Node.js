0ec4ccb8561af6a413a31936116a47f7c0f58a3e
Ivan-hwh <huweihong222@gmail.com>
2019-11-25 21:09:57 +0800

    第三方模块moment的使用

--- 06.第三方模块moment的使用.js
+++ 06.第三方模块moment的使用.js
@@ -1,1 +1,9 @@
+// 引入第三方模块moment
 var moment = require('moment');
+
+// 显示星期几
+var time1 = moment().weekday(); 
+// 格式化显示当前时间
+var time2 = moment().format('YYYY-MM-DD HH:mm:ss');
+console.log(time1);
+console.log(time2);
